134738eca8f0279e832e83e3398adce360cbe6db
yingweiy <yingweiy@gmail.com>
2017-08-09 20:27:18 -0500

    LeNet Code

---
+++ src/CNN/LeNetLab.py
@@ -0,0 +1,137 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import random
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from tensorflow.contrib.layers import flatten
+
+# Load Data
+mnist = input_data.read_data_sets("MNIST_data/", reshape=False)
+X_train, y_train           = mnist.train.images, mnist.train.labels
+X_validation, y_validation = mnist.validation.images, mnist.validation.labels
+X_test, y_test             = mnist.test.images, mnist.test.labels
+
+assert(len(X_train) == len(y_train))
+assert(len(X_validation) == len(y_validation))
+assert(len(X_test) == len(y_test))
+
+print()
+print("Image Shape: {}".format(X_train[0].shape))
+print()
+print("Training Set:   {} samples".format(len(X_train)))
+print("Validation Set: {} samples".format(len(X_validation)))
+print("Test Set:       {} samples".format(len(X_test)))
+
+import numpy as np
+
+# Pad images with 0s
+X_train = np.pad(X_train, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+X_validation = np.pad(X_validation, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+X_test = np.pad(X_test, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+
+print("Updated Image Shape: {}".format(X_train[0].shape))
+
+# Visualization
+index = random.randint(0, len(X_train))
+image = X_train[index].squeeze()
+#plt.figure(figsize=(1,1))
+#plt.imshow(image, cmap="gray")
+print(y_train[index])
+
+# Preprocess data
+X_train, y_train = shuffle(X_train, y_train)
+
+# Tensorflow
+EPOCHS = 10
+BATCH_SIZE = 128
+
+def conv2d(input, filter_size, height=1, nchannels=1, stride=1, padding = 'VALID', mu=0, sigma = 0.1):
+    F_W = tf.Variable(tf.random_normal([filter_size, filter_size, height, nchannels], mean=mu, stddev=sigma))
+    F_b = tf.Variable(tf.zeros([nchannels]))
+    strides = [1, stride, stride, 1]
+    return tf.nn.relu(tf.nn.conv2d(input, F_W, strides, padding) + F_b)
+
+def maxpool(input):
+    ksize = [1, 2, 2, 1]
+    strides = [1, 2, 2, 1]
+    padding = 'VALID'
+    return tf.nn.max_pool(input, ksize, strides, padding)
+
+
+def fully_connected(input, n_output, mu=0, sigma=0.1, activation=True):
+    n_input = input.get_shape()[1].value
+    W = tf.Variable(tf.truncated_normal(shape=(n_input, n_output), mean=mu, stddev=sigma))
+    b = tf.Variable(tf.zeros(n_output))
+    net = tf.matmul(input, W) + b
+    if activation==True:
+        net = tf.nn.relu(net)
+    return net
+
+
+def LeNet(x):
+    net = conv2d(x, 5, 1, 6)
+    net = maxpool(net)
+    net = conv2d(net, 5, 6, 16)
+    net = maxpool(net)
+    net = flatten(net)
+    net = fully_connected(net, 120)
+    net = fully_connected(net, 84)
+    net = fully_connected(net, 10, activation=False)
+    return net
+
+x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, 10)
+
+# Training settings
+rate = 0.001
+logits = LeNet(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
+
+# Model Evaluation settings
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+# Train
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+
+        validation_accuracy = evaluate(X_validation, y_validation)
+        print("EPOCH {} ...".format(i + 1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+
+    saver.save(sess, './lenet')
+    print("Model saved")
+
+# Evaluate
+with tf.Session() as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('.'))
+
+    test_accuracy = evaluate(X_test, y_test)
+    print("Test Accuracy = {:.3f}".format(test_accuracy))
