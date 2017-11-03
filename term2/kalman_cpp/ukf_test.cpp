0cd00d9fb2cc791946df91ab4c4dc5d9b95e3992
yyu17 <yyu17@slb.com>
2017-11-03 16:35:48 -0500

    added ukf code

--- term2/kalman_cpp/ukf_test.cpp
+++ term2/kalman_cpp/ukf_test.cpp
@@ -20,9 +20,31 @@ int main() {
 /*******************************************************************************
 * Programming assignment calls
 *******************************************************************************/
+    MatrixXd Xsig = MatrixXd(5, 11);
+    ukf.GenerateSigmaPoints(&Xsig);
+
+    //print result
+    std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+
 
     MatrixXd Xsig_aug = MatrixXd(7, 15);
     ukf.AugmentedSigmaPoints(&Xsig_aug);
+
+    MatrixXd Xsig_pred = MatrixXd(15, 5);
+    ukf.SigmaPointPrediction(&Xsig_pred);
+
+    VectorXd x_pred = VectorXd(5);
+    MatrixXd P_pred = MatrixXd(5, 5);
+    ukf.PredictMeanAndCovariance(&x_pred, &P_pred);
+
+    VectorXd z_out = VectorXd(3);
+    MatrixXd S_out = MatrixXd(3, 3);
+    ukf.PredictRadarMeasurement(&z_out, &S_out);
+
+    VectorXd x_out = VectorXd(5);
+    MatrixXd P_out = MatrixXd(5, 5);
+    ukf.UpdateState(&x_out, &P_out);
+
 
     return 0;
 }
